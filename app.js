6db0cadfa6b281b9573bde6c4fa32b841199155b
Aleksandr <loskutov@aleksandrl.ru>
2022-07-26 13:40:20 +0300

    remove unnecessary

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const http = require("http");
 const PORT = 8080;
 const app = express();
 app.use(express.json());
@@ -9,14 +7,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(__dirname, "dist")));
 
 const indexPath = path.join(__dirname, "dist", "index.html");
+
 app.get("*", (req, res) => {
   res.sendFile(indexPath);
 });
 
 const start = async () => {
   try {
-    http.createServer(app).listen(PORT, () => {
-      console.log("http server started on port", PORT);
+    app.listen(PORT, () => {
+      console.log("server started on port", PORT);
     });
   } catch (e) {
     console.log("ошибка", e);
