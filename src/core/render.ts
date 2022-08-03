8b01efce3c1d21891bd5283c25ee0d312945f31d
Aleksandr <loskutov@aleksandrl.ru>
2022-08-03 23:06:01 +0300

    Move

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,4 +1,6 @@
-export default function render(component, query = "root") {
+import Component from "core/component";
+
+export default function render(component: Component, query = "root") {
   const root = document.getElementById(query);
   if (root) {
     root.appendChild(component.element);
