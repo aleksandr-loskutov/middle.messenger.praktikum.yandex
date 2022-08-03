304469779a8a74d94722c9b187bbb111152ea817
Aleksandr <loskutov@aleksandrl.ru>
2022-08-03 23:03:01 +0300

    Remove

--- src/components/input/input.template.js
+++
@@ -1,4 +0,0 @@
-export default `
-{{#if label}}<label for="{{id}}" class="{{#if labelClass}}{{labelClass}}{{else}}form__label{{/if}}">{{label}}</label>{{/if}}
-<input name="{{name}}" type="{{#if type}}{{type}}{{else}}text{{/if}}" class="{{#if class}}{{class}}{{else}}form-input{{/if}}" value="{{value}}" id="{{id}}" placeholder="{{placeholder}}" {{#if disabled}}disabled{{/if}}/>
-`;
