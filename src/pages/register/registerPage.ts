b0e8a924dc00a599f3aea5747bbac6c96d66f6ed
Aleksandr <loskutov@aleksandrl.ru>
2022-08-06 22:08:50 +0300

    Add login & reg. pages

---
+++ src/pages/register/registerPage.ts
@@ -0,0 +1,51 @@
+import Component from "core/component";
+import { ValidationField, validateData } from "utils/validator";
+import { getValuesFromElements } from "utils/dom";
+
+export class RegisterPage extends Component {
+  static componentName = "RegisterPage";
+
+  constructor(props) {
+    super({
+      ...props,
+      onRegister: (e) => {
+        e.preventDefault();
+        const registrationData = getValuesFromElements.bind(this)(
+          "login",
+          "password",
+          "email",
+          "first_name",
+          "second_name",
+          "phone"
+        );
+        if (validateData.bind(this)(registrationData)) {
+          console.log("Данные регистрации:", registrationData);
+        }
+      }
+    });
+  }
+  render() {
+    // language=hbs
+    return `
+        <main class="main-auth">
+            <div class="login-box">
+                <h1 class="login-box__title">Регистрация</h1>
+                <form method="post" class="login-form">
+                    <div class="register-form__fields-block">
+                        {{{ControlledInput name="email" type="email" id="email" label="Почта" ref="email" validationField = "${ValidationField.Email}"}}}
+                        {{{ControlledInput type="text" name="login" id="login" label="Логин"  ref="login" validationField = "${ValidationField.Login}"}}}
+                        {{{ControlledInput type="text" name="first_name" id="first_name" label="Имя" ref="first_name" validationField = "${ValidationField.FirstName}"}}}
+                        {{{ControlledInput type="text" name="second_name" id="second_name" label="Фамилия" ref="second_name" validationField = "${ValidationField.SecondName}"}}}
+                        {{{ControlledInput type="tel" name="phone" id="phone" label="Телефон" error="Неверный номер телефона" ref="phone" validationField = "${ValidationField.Phone}"}}}
+                        {{{ControlledInput type="password" name="password" id="password" label="Пароль" error="Пароль должен быть не менее 6 символов" ref="password" validationField = "${ValidationField.Password}"}}}
+                        {{{ControlledInput type="password" name="password2" id="password2" label="Пароль (еще раз)" error="Пароли не совпадают" ref="password2" validationField = "${ValidationField.Password}"}}}
+                    </div>
+                    <div class="login-form__buttons-block">
+                        {{{Button type="submit" text="Зарегистрироваться" id="register-button" onClick=onRegister}}}
+                        <a href="/login" class="login-form__link">Уже есть аккаунт? Войти</a>
+                    </div>
+                </form>
+            </div>
+        </main>`;
+  }
+}
