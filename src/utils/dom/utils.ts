05c7a3964985d70f53aac48589693d9d14bb270b
Aleksandr <loskutov@aleksandrl.ru>
2022-08-06 21:59:12 +0300

    Add DOM utils

---
+++ src/utils/dom/utils.ts
@@ -0,0 +1,57 @@
+export function toggleOptionsWindow() {
+  document.getElementById("chat-options").classList.toggle("hidden");
+  document
+    .getElementById("chat-options-button")
+    .classList.toggle("options-button-active");
+}
+export function toggleAttachWindow() {
+  document.getElementById("attach-options").classList.toggle("hidden");
+}
+
+export function addUserModal() {
+  const modal = document.getElementById("modal-add-user");
+  modal.style.display = "block";
+}
+export function deleteUserModal() {
+  const modalDeleteUser = document.getElementById("modal-delete-user");
+  modalDeleteUser.style.display = "block";
+}
+export function addListeners() {
+  window.onclick = function (event) {
+    if (window.location.pathname === "/chat") {
+      const modalAddUser = document.getElementById("modal-add-user");
+      const modalDeleteUser = document.getElementById("modal-delete-user");
+      if (modalAddUser || modalDeleteUser) {
+        if (event.target.classList.contains("modal__wrapper")) {
+          modalAddUser.style.display = "none";
+          modalDeleteUser.style.display = "none";
+        }
+      }
+      //todo chat-options & attach-options
+      const chatOptions = document.getElementById("chat-options");
+      const attachOptions = document.getElementById("attach-options");
+    }
+    if (window.location.pathname === "/profile") {
+      const personalImage = document.getElementById("user-avatar");
+      const input = document.getElementById("avatar");
+      personalImage.addEventListener("click", () => {
+        input.click();
+      });
+    }
+  };
+}
+
+export function getValuesFromElements(...names: string[]): {} {
+  const data = {};
+  names.forEach((name) => {
+    const element = this.element?.querySelector(
+      name.includes("#") ? name : `[name="${name}"]`
+    );
+    if (element) {
+      name.includes("#")
+        ? (data[element.getAttribute("name")] = element.value)
+        : (data[name] = element.value);
+    }
+  });
+  return data;
+}
