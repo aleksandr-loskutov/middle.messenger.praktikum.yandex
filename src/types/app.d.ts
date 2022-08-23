1127e2a446c47af6204a31345fac9954e24e563c
Aleksandr <loskutov@aleksandrl.ru>
2022-08-23 23:02:11 +0300

    Update global types

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,7 +1,32 @@
+import { Store, Router } from "core";
+
 declare global {
   export type Nullable<T> = T | null;
   export type Keys<T extends Record<string, unknown>> = keyof T;
   export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+  export type Indexed = { [key: string]: any };
+
+  export type AppState = {
+    appIsInited: boolean;
+    isLoading: boolean;
+    loginError: string | null;
+    user: User | null;
+  };
+
+  export type User = {
+    id: number;
+    login: string;
+    firstName: string;
+    secondName: string;
+    displayName: string;
+    avatar: string;
+    phone: string;
+    email: string;
+  };
+  export interface Window {
+    store: Store<AppState>;
+    router: Router;
+  }
 }
 
 export {};
