250e9120e1465a49c722279c3aebc084bc938e77
Aleksandr <loskutov@aleksandrl.ru>
2022-08-25 13:27:48 +0300

    Update main index

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import Handlebars from "handlebars";
-import {
-  ChatPage,
-  LoginPage,
-  ErrorPage,
-  RegisterPage,
-  ProfilePage,
-  PasswordChangePage
-} from "pages";
 import "css/style.scss";
 import registerComponent from "core/registerComponent";
-import mockData from "utils/mock";
 import components from "components";
-import { Router } from "core";
+import { Router, Store } from "core";
+import { defaultState } from "./store";
+import { initApp } from "services";
+import { initRouter } from "./router";
 
 Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
   return arg1 == arg2 ? options.fn(this) : options.inverse(this);
@@ -20,13 +14,31 @@ Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
 for (const [, component] of Object.entries(components)) {
   registerComponent(component);
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const store = new Store<AppState>(defaultState);
+  const router = new Router();
 
-const router = new Router();
+  window.router = router;
+  window.store = store;
 
-router.use("/", LoginPage);
-router.use("/sign-up", RegisterPage);
-router.use("/messenger", ChatPage, mockData);
-router.use("/settings", ProfilePage, { user: { ...mockData.user } });
-router.use("/password", PasswordChangePage);
-router.use("*", ErrorPage);
-router.start();
+  /**
+   * Инициализируем роутер
+   */
+  initRouter(router, store);
+
+  store.on("changed", (prevState, nextState) => {
+    router.start();
+    if (process.env.DEBUG) {
+      console.log(
+        "%cstore updated",
+        "background: #222; color: #bada55",
+        nextState
+      );
+    }
+  });
+
+  /**
+   * Загружаем данные для приложения
+   */
+  store.dispatch(initApp);
+});
