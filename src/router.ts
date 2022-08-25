d3ce4e93c216de3c00ea282a429573f306947ec8
Aleksandr <loskutov@aleksandrl.ru>
2022-08-25 11:48:58 +0300

    Add init router

---
+++ src/router.ts
@@ -0,0 +1,41 @@
+import { Router, Store } from "core";
+import { getPageComponent, Pages } from "pages";
+
+const routes = [
+  {
+    path: "/",
+    component: Pages.Login,
+    shouldAuthorized: false
+  },
+  {
+    path: "/sign-up",
+    component: Pages.Register,
+    shouldAuthorized: false
+  },
+  {
+    path: "/messenger/:id",
+    component: Pages.Messenger,
+    shouldAuthorized: true
+  },
+  {
+    path: "/password",
+    component: Pages.Password,
+    shouldAuthorized: true
+  },
+  {
+    path: "/settings",
+    component: Pages.Profile,
+    shouldAuthorized: true
+  },
+  {
+    path: "*",
+    component: Pages.Error,
+    shouldAuthorized: false
+  }
+];
+
+export function initRouter(router: Router, store: Store<AppState>) {
+  routes.forEach((route) => {
+    router.use(route.path, getPageComponent(route.component));
+  });
+}
