ff6628b6ca51bf2048ce4943d0d17a387f29c957
Aleksandr <loskutov@aleksandrl.ru>
2022-08-25 11:35:29 +0300

    Add HOCs

---
+++ src/components/hoc/withRouter.ts
@@ -0,0 +1,18 @@
+import { Component } from "core";
+import { Router } from "core";
+
+type WithRouterProps = { router: Router };
+
+export function withRouter<P extends WithRouterProps>(
+  WrappedBlock: Component<P>
+) {
+  // @ts-expect-error No base constructor has the specified number of type arguments
+  return class extends WrappedBlock<P> {
+    public static componentName =
+      WrappedBlock.componentName || WrappedBlock.name;
+
+    constructor(props: P) {
+      super({ ...props, router: window.router });
+    }
+  } as Component<Omit<P, "router">>;
+}
