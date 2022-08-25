a2e850922d89fd08136577d40424d8677d6b4a43
Aleksandr <loskutov@aleksandrl.ru>
2022-08-25 21:58:00 +0300

    Add transformers

---
+++ src/utils/apiTransformers.ts
@@ -0,0 +1,21 @@
+import { UserDTO } from "types/api";
+
+export const transformUser = (data: UserDTO): User => {
+  return {
+    id: data.id,
+    login: data.login,
+    firstName: data.first_name,
+    secondName: data.second_name,
+    displayName: data.display_name,
+    avatar: data.avatar,
+    phone: data.phone,
+    email: data.email
+  };
+};
+
+export const transformPasswords = (data) => {
+  return {
+    oldPassword: data.password,
+    newPassword: data.new_password
+  };
+};
