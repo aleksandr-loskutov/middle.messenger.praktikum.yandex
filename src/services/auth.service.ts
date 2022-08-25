25b3c716af76a4a07c9cb7099f535033b98c0a89
Aleksandr <loskutov@aleksandrl.ru>
2022-08-25 22:06:38 +0300

    Add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,59 @@
+import { AuthAPI } from "api";
+import type { Dispatch } from "core";
+import { transformUser, apiHasError } from "utils";
+import {
+  LoginData as LoginPayload,
+  RegisterData as RegisterPayload
+} from "types/api";
+
+export const login = async (
+  dispatch: Dispatch<AppState>,
+  state: AppState,
+  payload: LoginPayload
+) => {
+  dispatch({ isLoading: true });
+  const api = new AuthAPI();
+  const response = await api.login(payload);
+  if (apiHasError(response)) {
+    dispatch({ isLoading: false, formError: response.reason });
+    return;
+  }
+  const user = await api.getUser();
+  dispatch({ isLoading: false, formError: null });
+  if (apiHasError(response)) {
+    dispatch(logout);
+    return;
+  }
+  dispatch({ user: transformUser(user) });
+  window.router.go("/messenger");
+};
+
+export const register = async (
+  dispatch: Dispatch<AppState>,
+  state: AppState,
+  payload: RegisterPayload
+) => {
+  dispatch({ isLoading: true });
+  const api = new AuthAPI();
+  const response = await api.register(payload);
+  if (apiHasError(response)) {
+    dispatch({ isLoading: false, formError: response.reason });
+    return;
+  }
+  const user = await api.getUser();
+  dispatch({ isLoading: false, formError: null });
+  if (apiHasError(response)) {
+    dispatch(logout);
+    return;
+  }
+  dispatch({ user: transformUser(user) });
+  window.router.go("/messenger");
+};
+
+export const logout = async (dispatch: Dispatch<AppState>) => {
+  dispatch({ isLoading: true });
+  const api: AuthAPI = new AuthAPI();
+  await api.logout();
+  dispatch({ isLoading: false, user: null });
+  window.router.go("/");
+};
