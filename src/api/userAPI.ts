be2f22923c116e23e4786bfdcb5e113257e4f892
Aleksandr <loskutov@aleksandrl.ru>
2022-08-27 22:29:34 +0300

    Update user api

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -1,25 +1,30 @@
 import { BaseAPI } from "./baseAPI";
-import { LoginData, RegisterData } from "types/api";
 
 export class UserAPI extends BaseAPI {
   constructor() {
     super("/user");
   }
 
-  updateUserData(data: LoginData): Promise<unknown> {
+  updateUserData(data): Promise<unknown> {
     return this.httpService.put("/profile", {
       data,
       headers: { "Content-Type": "application/json" }
     });
   }
-  updateUserPassword(data: RegisterData): Promise<unknown> {
+  updateUserPassword(data): Promise<unknown> {
     return this.httpService.put("/password", {
       data,
       headers: { "Content-Type": "application/json" }
     });
   }
-  updateUserAvatar(data: RegisterData): Promise<unknown> {
+  updateUserAvatar(data): Promise<unknown> {
     return this.httpService.put("/profile/avatar", {
+      data,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+  searchUsers(data): Promise<unknown> {
+    return this.httpService.post("/search", {
       data,
       headers: { "Content-Type": "application/json" }
     });
