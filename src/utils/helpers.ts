a1b749510dfde71f4b97cdf62ee337a3b7690146
Aleksandr <loskutov@aleksandrl.ru>
2022-08-27 22:17:25 +0300

    Add new utils

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -52,3 +52,92 @@ export function logger(message: string, data?: object) {
     console.log(message, data);
   }
 }
+
+export const getRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const getInitials = (name) => {
+  let initials;
+  const nameSplit = name.split(" ");
+  const nameLength = nameSplit.length;
+  if (nameLength > 1) {
+    initials =
+      nameSplit[0].substring(0, 1) + nameSplit[nameLength - 1].substring(0, 1);
+  } else if (nameLength === 1) {
+    initials = nameSplit[0].substring(0, 1);
+  } else return;
+
+  return initials.toUpperCase();
+};
+
+export const createImageFromInitials = (
+  size,
+  name,
+  color = getRandomColor()
+) => {
+  if (name == null) return;
+  name = getInitials(name);
+
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  canvas.width = canvas.height = size;
+
+  context.fillStyle = "#ffffff";
+  context.fillRect(0, 0, size, size);
+
+  context.fillStyle = `${color}25`;
+  context.fillRect(0, 0, size, size);
+
+  context.fillStyle = color;
+  context.textBaseline = "middle";
+  context.textAlign = "center";
+  context.font = `${size / 2}px Roboto`;
+  context.fillText(name, size / 2, size / 2);
+
+  return canvas.toDataURL();
+};
+
+export function displayDate(data) {
+  const date = new Date(data);
+  const dateNow = new Date();
+  const yearDif = dateNow.getFullYear() - date.getFullYear();
+  if (yearDif === 0) {
+    const dayDif = dateNow.getDay() - date.getDay();
+    if (dayDif === 0) {
+      const hourDif = dateNow.getHours() - date.getHours();
+      if (hourDif === 0) {
+        const minutesDif = dateNow.getMinutes() - date.getMinutes();
+
+        if (minutesDif >= 0 && minutesDif < 5) return "1 минуту назад";
+        if (minutesDif >= 5 && minutesDif < 10) return "5 минут назад";
+        if (minutesDif >= 10 && minutesDif < 30) {
+          return "10 минут назад";
+        }
+        return "30 минут назад";
+      }
+      return `${date.getHours()}:${date.getMinutes()}`;
+    }
+
+    return `${date.getDay()} ${date.toLocaleString("default", {
+      month: "long"
+    })}`;
+  }
+  return (
+    date.getFullYear() + "." + (date.getMonth() + 1) + "_" + date.getDate()
+  );
+}
+
+export function setDefaultAvatars(chats: any[]) {
+  chats.forEach((chat) => {
+    if (!chat.avatar) {
+      chat.avatar = createImageFromInitials(100, chat.title);
+    }
+  });
+  return chats;
+}
