f45c001ed5d8b81cbfe5606c14e857484501857c
Aleksandr <loskutov@aleksandrl.ru>
2022-09-07 16:39:01 +0300

    Update message service

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -2,12 +2,13 @@ import { ChatAPI } from "api";
 import type { Dispatch } from "core";
 import { logger, connectWebSocket } from "utils";
 import { WS_ENDPOINT } from "utils/consts";
+import { ChatDTO, MessageDTO, Token } from "types/api";
 
 export async function connectChats(
   dispatch: Dispatch<AppState>,
   state: AppState
 ) {
-  if (state.isChatsLoaded || state.isChatsLoading) return;
+  if (state.isChatsLoaded || state.isChatsLoading || !state.chats) return;
   dispatch({ isChatsLoading: true, isLoading: true });
   //получаем список чатов
   const chats = state.chats.map((chat) => {
@@ -17,7 +18,7 @@ export async function connectChats(
   const api = new ChatAPI();
   const chatsWithTokens = await Promise.all(
     chats.map(async (chat) => {
-      const { token } = await api.getWsTokenForChat(chat.chatId);
+      const { token } = (await api.getWsTokenForChat(chat.chatId)) as Token;
       return {
         ...chat,
         token
@@ -27,13 +28,14 @@ export async function connectChats(
   //для каждого чата подключаемся к вебсокету
   const chatsWithSockets = await Promise.all(
     chatsWithTokens.map(async (chat) => {
-      const socket = await connectChatToSocket.bind(this)(
+      const socket = (await connectChatToSocket.bind(this)(
         chat.chatId,
         chat.token
-      );
+      )) as WebSocket;
       return { ...chat, socket };
     })
   );
+  //todo - сделать проверку на то, что все чаты подключены
   dispatch({
     isChatsLoaded: true,
     isChatsLoading: false,
@@ -42,13 +44,16 @@ export async function connectChats(
   });
 }
 
-export async function connectChatToSocket(chatId, token) {
+export async function connectChatToSocket(
+  chatId: number,
+  token: string
+): Promise<WebSocket | undefined> {
   const userId = this.props.store.getState().user.id;
-  const currentChatId = this.props.idParam;
+
   try {
-    const socket = (await connectWebSocket(
+    const socket = await connectWebSocket(
       `${WS_ENDPOINT}${userId}/${chatId}/${token}`
-    )) as WebSocket;
+    );
 
     //инициирующий запрос на получение последних 20 сообщений
     socket.send(
@@ -60,26 +65,42 @@ export async function connectChatToSocket(chatId, token) {
 
     socket.addEventListener("message", (event) => {
       let { data } = event;
-      if (data) data = JSON.parse(data);
-      const { type, content } = data;
-
+      if (data) {
+        data = JSON.parse(data);
+      } else return;
+      const { type, content, user_id } = data as MessageDTO;
       if (type === "pong") return;
+      const { chats } = this.props.store.getState();
       const { chatMessages } = this.props.store.getState();
+      const currentChatId = this.props.idParam;
       const currentChatMessages = chatMessages[chatId] || [];
-      const currentChat = this.props.store
-        .getState()
-        .chats.find((chat) => chat.id === chatId);
-      // const chats = this.props.store.getState().chats;
+      const updatedChats = chats.map((chat: ChatDTO) => {
+        if (chat.id === chatId) {
+          if (user_id !== userId) {
+            return {
+              ...chat,
+              unread_count:
+                currentChatId !== chat.id
+                  ? chat.unread_count + 1
+                  : chat.unread_count,
+              last_message: { ...data, isMine: false }
+            };
+          } else {
+            return {
+              ...chat,
+              last_message: { ...data, content: "Вы: " + content, isMine: true }
+            };
+          }
+        } else {
+          return chat;
+        }
+      });
       if (!Array.isArray(data) && data) {
         switch (type) {
           case "message":
             logger("Получено новое сообщение в чате", content);
-            currentChat.last_message = data;
-            if (currentChatId !== chatId) {
-              currentChat.unread_count = currentChat.unread_count + 1;
-            }
             this.props.store.dispatch({
-              // chats: [...chats],
+              chats: updatedChats,
               chatMessages: {
                 ...chatMessages,
                 [chatId]: [data, ...currentChatMessages]
@@ -111,4 +132,12 @@ export async function connectChatToSocket(chatId, token) {
   } catch (error) {
     logger("Ошибка в работе с вебсокетом", error);
   }
+  return;
+}
+
+export function setReaded(_dispatch: Dispatch<AppState>, state: AppState) {
+  const { chats } = state;
+  const currentChatId = this.props.idParam;
+  const chat = chats?.find((chat) => chat.id === currentChatId);
+  if (chat) chat.unread_count = 0;
 }
